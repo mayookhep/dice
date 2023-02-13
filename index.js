9cfcf0e89ea68416f139ad540301608a2901d16e
mayookhep <53192084+mayookhep@users.noreply.github.com>
2023-02-13 09:05:33 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 var randomNumber= Math.floor(Math.random()*6)+1;
 var randomDiceimage="dice"+randomNumber+".png";
-var randomDiceimageSrc="images/"+randomDiceimage;
+var randomDiceimageSrc=randomDiceimage;
 var image1 = document.querySelectorAll("img")[0]
 image1.setAttribute("src",randomDiceimageSrc);
 
 var randomNumber2= Math.floor(Math.random()*6)+1;
 var randomDiceimage2="dice"+randomNumber2+".png";
-var randomDiceimageSrc2="images/"+randomDiceimage2;
+var randomDiceimageSrc2=randomDiceimage2;
 var image2 = document.querySelectorAll("img")[1]
 image2.setAttribute("src",randomDiceimageSrc2);
 if (randomNumber>randomNumber2)
